5581b704d35c41f27ea9149848865d2752dc6184
Leonid Romanov <romanovsparta@ya.ru>
2022-07-11 22:12:59 +0300

    master

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,24 +1,16 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
+import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Leonid", "Romanov", (byte) 34));
-        users.add(new User("Михаил", "Горбачев", (byte) 11));
-        users.add(new User("Серафим", "Бадиков", (byte) 27));
-        users.add(new User("Гурам", "Гевондян", (byte) 37));
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
-        users.forEach(u -> userDao.saveUser(u.getName(), u.getLastName(), u.getAge()));
-        userDao.getAllUsers().forEach(System.out::println);
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+    public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Leonid", "Romanov", (byte) 34);
+        userService.saveUser("Михаил", "Горбачев", (byte) 11);
+        userService.saveUser("Серафим", "Бадиков", (byte) 27);
+        userService.saveUser("Гурам", "Гевондян", (byte) 37);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,11 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.*;
 import jm.task.core.jdbc.model.User;
-
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+    UserDao userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
         userDao.createUsersTable();
     }
